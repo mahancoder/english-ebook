0bac1d8c3a6fa184622a583a5150946a0cbba387
Mahan Lamee <mahan.lameie87@gmail.com>
2021-12-23 16:08:11 +0330

    Added sound system

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,6 +1,7 @@
 const iframe = document.getElementById("frame");
 var page = 1;
-
+var audio = new Audio();
+var playing = false;
 function fade(ev) {
     iframe.contentWindow.document.getElementsByClassName("center")[0].style.opacity = 0;
 }
@@ -9,6 +10,9 @@ function load(ev) {
     setTimeout(() => {
         iframe.setAttribute("src", `pages/${page}.html`);
     }, 500);
+    audio = new Audio(`sounds/${page}.mp3`);
+    audio.addEventListener("loadeddata", () => { document.getElementById("sound").style.backgroundImage = "url(\"wwwroot/sound.png\")"; document.getElementById("sound").disabled = false; } );
+    audio.addEventListener("ended", () => { document.getElementById("sound").style.backgroundImage = "url(\"wwwroot/sound.png\")"; playing=false; } );
 }
 
 function nextPage(ev) {
@@ -27,6 +31,19 @@ function prevPage(ev) {
     }
     load();
 }
+function toggle() {
+    if (playing) {
+        audio.pause();
+        playing = false;
+        document.getElementById("sound").style.backgroundImage = "url(\"wwwroot/sound.png\")";
+    }
+    else {
+        audio.play();
+        playing = true;
+        document.getElementById("sound").style.backgroundImage = "url(\"wwwroot/pause.png\")";
+    }
+}
 document.body.onload = load;
 document.getElementById("next").onclick = nextPage;
 document.getElementById("prev").onclick = prevPage;
+document.getElementById("sound").onclick = toggle;
